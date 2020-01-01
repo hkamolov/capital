795225db4d972d27a186d9d5cedb78f7c141da00
hkamolov <hkamolov@users.noreply.github.com>
2020-01-02 03:24:24 +0500

    Added range function to get range values of dates
    Gets only the transaction values within two inserted dates (start and end dates)

---
+++ export_file.py
@@ -0,0 +1,44 @@
+import csv
+import os
+# from tempfile import NamedTemporaryFile
+# import shutil
+import sys
+from pathlib import Path
+import datetime
+
+                      
+# https://stackoverflow.com/questions/6899101/select-a-range-of-dates-in-python-dictionary
+# returns list[dict, dict, dict, ...] just filtered from sorted list 
+def get_range_dates(sortedlist, start_date, end_date):
+    range_list=[]
+    for field in sortedlist:
+        if start_date <= field['Date'] <= end_date:
+            range_list.append(dict(field))
+    print('RANGE LIST STARTS HERE: \n', range_list)
+    return range_list        
+
+
+# fieldnames = ['Date', 'Income/Expenses', 'Category', 'Memo', 'Amount'] or headlines parameter
+def create_file(start_date, end_date, headlines, sortedlist, name_file):
+
+    # To get transactions only within range(start_date, end_date) of sorted list 
+    # format: list[dict, dict, dict, ...]
+    my_range = get_range_dates(sortedlist, start_date, end_date)
+
+    # FOLDER CREATION IF NOT EXIST: 
+    # REPOSITORY for a line below: https://www.bogotobogo.com/python/python_files.php
+    foldername = os.path.join(os.path.expanduser('~'), 'Documents', 'Capital')
+
+    # https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
+    Path(foldername).mkdir(parents = True, exist_ok=True)
+    
+    # REPOSITORY for a line below: https://www.bogotobogo.com/python/python_files.php
+    pathname = os.path.join(foldername, name_file)
+    # filename = rawfilename.replace('\\\\', '\\')
+    
+    with open(pathname, 'w', newline='') as new_file:
+        csv_writer = csv.DictWriter(new_file, fieldnames=headlines)
+        csv_writer.writeheader()
+
+        # for head in headlines:
+        csv_writer.writerows(my_range)
